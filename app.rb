118a68f4f3ee53cc332fbefc287846dd9828235a
Mohamed Warsame <mwarsame9@gmail.com>
2017-05-05 11:31:42 -0700

    create integration tests, views pages

---
+++ app.rb
@@ -0,0 +1,43 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/project")
+require("./lib/volunteer")
+require("pg")
+require("pry")
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get("/") do
+  erb(:index)
+end
+
+get("/projects/new") do
+  erb(:project_form)
+end
+
+post("/projects") do
+  name = params.fetch("name")
+  project = Project.new({:id => nil, :name => name})
+  project.save()
+  erb(:success)
+end
+
+get('/projects') do
+  @projects = Project.all()
+  erb(:projects)
+end
+
+get("/projects/:id") do
+  @project = Project.find(params.fetch("id").to_i())
+  erb(:project)
+end
+
+post("/volunteers") do
+  name = params.fetch("name")
+  project_id = params.fetch("project_id").to_i()
+  @project = Project.find(project_id)
+  @volunteer = Volunteer.new({:name => name, :project_id => project_id})
+  @volunteer.save()
+  erb(:success)
+end
